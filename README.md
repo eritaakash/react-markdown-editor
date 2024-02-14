a317c209085bee68d5bbd4f4657319339340a32c
eritaakash <eritaakash@gmail.com>
2024-02-14 18:27:44 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+## React Markdown Editor
+React Component to render markdown preview directly into text area.
+
+```bash
+npm i @eritaakash/react-markdown-editor
+```
+
+![React Markdown Editor Preview](https://media.discordapp.net/attachments/695932896560676935/1207306730531201065/image.png?ex=65df2b03&is=65ccb603&hm=b0b031a60d5d55dea2cfd86bf0d4f8be855d19cd5a07e3358c1b9c6e6d412d96&=&format=webp&quality=lossless)
+
+### Features
+It's currently very basic, and all it does is:
+
+- Provide a live preview within the text field. The user won't have to switch between "editor" and "preview" section.
+- Format Bold, Italic and Underline texts as of now.
+- Provide option for customizable styling of the text editor container.
+- Provide option to customize the css of the preview container and markdown textarea.
+
+### Todo
+- Add support for various other markdown formatting methods such as List Item, Link & Image.
+
+### Contributing 
+Reporting of issues and pull requests are welcomed.
---
+++ src/index.js
@@ -0,0 +1,77 @@
+import { useState, useEffect } from 'react';
+import styles from './editor-styling.module.css';
+
+const MarkdownEditor = ({ containerStyle = styles.containerStyle, text, setText }) => {
+    const [output, setOutput] = useState('');
+    const dimStyle = 'opacity:0.5; font-weight:200;';
+
+    const convertMarkdownToHtml = (markdownText) => {
+        const regex = {
+            bold: /(?<=\W|^)\*\*([^\s].*?[^\s])\*\*(?=\W|$)/g,
+            italic: /(?<=\W|^)\_([^\s].*?[^\s])\_(?=\W|$)/g,
+            underline: /(?<=\W|^)\_\_([^\s].*?[^\s])\_\_(?=\W|$)/g,
+            strikethrough: /(?<=\W|^)~~([^\s].*?[^\s])~~(?=\W|$)/g,
+        };
+
+        let htmlText = text;
+
+        /* New Line */
+        htmlText = htmlText.replace(
+            /\n/g,
+            `<br>`
+        );
+
+        /* Bold Text */
+        htmlText = htmlText.replace(
+            regex.bold,
+            `<strong><span style="${dimStyle}">**</span>$1<span style="${dimStyle}">**</span></strong>`
+        );
+
+        /* Underline Text */
+        htmlText = htmlText.replace(
+            regex.underline,
+            `<u><span style="${dimStyle}">__</span>$1<span style="${dimStyle}">__</span></u>`
+        );
+
+        /* Italic Text */
+        htmlText = htmlText.replace(
+            regex.italic,
+            `<em><span style="${dimStyle}">_</span>$1<span style="${dimStyle}">_</span></em>`
+        );
+
+
+        /* Strikethrough Text */
+        htmlText = htmlText.replace(
+            regex.strikethrough,
+            `<del><span style="${dimStyle}">~~</span>$1<span style="${dimStyle}">~~</span></del>`
+        );
+
+        return htmlText;
+    };
+
+    const handleInputChange = (value) => {
+        setText(value);
+    };
+
+    useEffect(() => {
+        const htmlText = convertMarkdownToHtml(text);
+        setOutput(htmlText);
+    }, [text]);
+
+    return (
+        <section className={containerStyle}>
+            <textarea
+                value={text}
+                onChange={(e) => handleInputChange(e.target.value)}
+                rows={10}
+                cols={50}
+                placeholder="Enter Markdown text..."
+                className={`${styles.previewContainerCommon} ${styles.textArea} markdown-editor`}
+            />
+            <div dangerouslySetInnerHTML={{ __html: output }}
+                className={`${styles.previewContainerCommon} ${styles.previewDiv} markdown-editor`} />
+        </section>
+    );
+};
+
+export default MarkdownEditor;
